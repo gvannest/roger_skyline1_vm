de51c921e6c43e7d7b458e0954c0ccc651f85145
Gautier Vanneste <gautier.vanneste42@gmail.com>
2019-08-28 08:42:28 -0500

    ssh config

--- setting_up.sh
+++ setting_up.sh
@@ -1,4 +1,6 @@
 sh install_script.sh
 cp -r ./interfaces /etc/network/
 cp -r ./interfaces.d/* /etc/network/interfaces.d/
+cp -r ./sshd_config /etc/ssh/
+
 iptables -A INPUT -p tcp --dport 2222 --jump ACCEPT

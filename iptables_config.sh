d0106eed1b3da2d2fbd7d3b663d0c280e01d15e2
Gautier Vanneste <gautier.vanneste42@gmail.com>
2019-09-05 12:39:24 -0500

    iptables done

--- iptables_config.sh
+++ iptables_config.sh
@@ -1,27 +1,62 @@
+#!/bin/sh
+
 # Flushing all rules
 iptables -F
 iptables -X
-# Setting default filter policy
+# Setting default filter policy : we drop everythong
 iptables -P INPUT DROP
 iptables -P OUTPUT DROP
 iptables -P FORWARD DROP
-# Allow unlimited traffic on loopback
+
+# Allow unlimited traffic on loopback (localhost)
 iptables -A INPUT -i lo -j ACCEPT
 iptables -A OUTPUT -o lo -j ACCEPT
 
+#ssh service
 iptables -A INPUT -p tcp --dport 2222 -j ACCEPT
 iptables -A OUTPUT -p tcp --sport 2222 -j ACCEPT
 
-iptables -A OUTPUT -p tcp --sport 80 -j ACCEPT
-iptables -A INPUT -p tcp --dport 80 -j ACCEPT
+#allow icmp requests (PING)
+iptables -A INPUT -p icmp -j ACCEPT
+iptables -A OUTPUT -p icmp -j ACCEPT
 
-iptables -A OUTPUT -p tcp --sport 53 -j ACCEPT
-iptables -A OUTPUT -p udp --sport 53 -j ACCEPT
-iptables -A INPUT -p tcp --dport 53 -j ACCEPT
-iptables -A INPUT -p udp --dport 53 -j ACCEPT
+#NTP (to allow server's clock to follow the time)
+iptables -A INPUT -p udp --dport 123 -j ACCEPT
 
-iptables -A OUTPUT -p tcp --sport 443 -j ACCEPT
-iptables -A INPUT -p tcp --dport 443 -j ACCEPT
+#allow DNS request and response
+iptables -A OUTPUT -p tcp --dport 53 -m conntrack --ctstate NEW,ESTABLISHED -j ACCEPT
+iptables -A OUTPUT -p udp --dport 53 -m conntrack --ctstate NEW,ESTABLISHED -j ACCEPT
+iptables -A INPUT -p tcp --sport 53 -m conntrack --ctstate ESTABLISHED -j ACCEPT
+iptables -A INPUT -p udp --sport 53 -m conntrack --ctstate ESTABLISHED -j ACCEPT
 
+#Accept web traffic HTTP & HTTPS
+# Incoming
+iptables -A OUTPUT -p tcp --sport 80 -m conntrack --ctstate ESTABLISHED -j ACCEPT
+iptables -A INPUT -p tcp --dport 80 -m conntrack --ctstate NEW,ESTABLISHED -j ACCEPT
+iptables -A OUTPUT -p tcp --sport 443 -m conntrack --ctstate ESTABLISHED -j ACCEPT
+iptables -A INPUT -p tcp --dport 443 -m conntrack --ctstate NEW,ESTABLISHED -j ACCEPT
+# Outgoing
+iptables -A OUTPUT -p tcp --dport 80 -m conntrack --ctstate NEW,ESTABLISHED -j ACCEPT
+iptables -A INPUT -p tcp --sport 80 -m conntrack --ctstate ESTABLISHED -j ACCEPT
+iptables -A OUTPUT -p tcp --dport 443 -m conntrack --ctstate NEW,ESTABLISHED -j ACCEPT
+iptables -A INPUT -p tcp --sport 443 -m conntrack --ctstate ESTABLISHED -j ACCEPT
+
+#Allow connezion to and from FTP servers
+iptables -A OUTPUT -p tcp --sport 20 -j ACCEPT
+iptables -A INPUT -p tcp --dport 20 -j ACCEPT
+iptables -A OUTPUT -p tcp --sport 21 -j ACCEPT
+iptables -A INPUT -p tcp --dport 21 -j ACCEPT
+
+#Allow all established connection to send and receive packets 
+iptables -A INPUT -m conntrack --ctstate ESTABLISHED -j ACCEPT
+iptables -A OUTPUT -m conntrack --ctstate ESTABLISHED -j ACCEPT
+
+#Packets which have not been catched by a previous rule will be dropped here
 iptables -A INPUT -j DROP
 iptables -A OUTPUT -j DROP
+iptables -A FORWARD -j DROP
+
+
+#savec changes
+iptables-save > /etc/iptables/rules.v4
+iptables-save > /etc/iptables/rules.v6
